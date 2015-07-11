bdc8ce4f56cd53f76bfb6a1440247c1f750e3fd7
Albert Puigsech <albert@puigsech.com>
2015-07-11 21:07:28 +0200

    add SetField support for AuditRuleData

--- _examples/addrule.go
+++ _examples/addrule.go
@@ -11,20 +11,22 @@ import (
 
 func main() {
 	al, _ := audit.OpenLink(0, 0)
-	al.Request(audit.AUDIT_LIST_RULES, []byte(""))
 
 	rule := &audit.AuditRuleData{
 		Flags:  audit.AUDIT_FILTER_EXIT,
 		Action: audit.AUDIT_ALWAYS,
 	}
 	rule.SetSyscall(syscall.SYS_RMDIR)
-	al.AddRule(rule)
-
-	rule = &audit.AuditRuleData{
-		Flags:  audit.AUDIT_FILTER_EXIT,
-		Action: audit.AUDIT_ALWAYS,
-	}
-	rule.SetSyscall(syscall.SYS_FORK)
+	rule.SetField(audit.AUDIT_PID, 1234, audit.AUDIT_EQUAL)
+	rule.SetField(audit.AUDIT_UID, 1000, audit.AUDIT_EQUAL)
+	rule.SetField(audit.AUDIT_EUID, 1000, audit.AUDIT_EQUAL)
+	rule.SetField(audit.AUDIT_SUID, 1000, audit.AUDIT_EQUAL)
+	rule.SetField(audit.AUDIT_FSUID, 1000, audit.AUDIT_EQUAL)
+	rule.SetField(audit.AUDIT_GID, 1000, audit.AUDIT_EQUAL)
+	rule.SetField(audit.AUDIT_EGID, 1000, audit.AUDIT_EQUAL)
+	rule.SetField(audit.AUDIT_SGID, 1000, audit.AUDIT_EQUAL)
+	rule.SetField(audit.AUDIT_FSGID, 1000, audit.AUDIT_EQUAL)
+	rule.SetField(audit.AUDIT_FILTERKEY, "TEST", audit.AUDIT_EQUAL)
 	al.AddRule(rule)
 
 	//al.DelRule(rule)
--- protocols/audit/ruledata.go
+++ protocols/audit/ruledata.go
@@ -27,8 +27,38 @@ func (rule *AuditRuleData) SetSyscall(scn int) error {
 	return nil
 }
 
+func (rule *AuditRuleData) SetField(field uint32, value interface{}, flags uint32) {
+	idx := rule.Field_count
+
+	if idx >= AUDIT_MAX_FIELDS {
+		return
+	}
+
+	switch field {
+	case AUDIT_PID, AUDIT_UID, AUDIT_EUID, AUDIT_SUID, AUDIT_FSUID, AUDIT_GID, AUDIT_EGID, AUDIT_SGID, AUDIT_FSGID:
+		if intvalue, ok := value.(int); ok {
+			rule.Fields[idx] = field
+			rule.Values[idx] = uint32(intvalue)
+			rule.Fieldflags[idx] = flags
+			rule.Field_count++
+		}
+	case AUDIT_FILTERKEY:
+		if strvalue, ok := value.(string); ok {
+			if field == AUDIT_FILTERKEY && len(strvalue) > AUDIT_MAX_KEY_LEN {
+				return
+			}
+			rule.Fields[idx] = field
+			rule.Values[idx] = uint32(len(strvalue))
+			rule.Fieldflags[idx] = flags
+			rule.Buflen = rule.Buflen + uint32(len(strvalue))
+			rule.Buf = append(rule.Buf, []byte(strvalue + "\x00")...)
+			rule.Field_count++
+		}
+	}
+}
+
 func (rule *AuditRuleData) toWireFormat() []byte {
-	b := make([]byte, int(unsafe.Sizeof(*rule))+int(rule.Buflen))
+	b := make([]byte, int(unsafe.Sizeof(*rule))-int(unsafe.Sizeof(rule.Buf))+len(rule.Buf))
 	*(*uint32)(unsafe.Pointer(&b[0:4][0])) = rule.Flags
 	*(*uint32)(unsafe.Pointer(&b[4:8][0])) = rule.Action
 	*(*uint32)(unsafe.Pointer(&b[8:12][0])) = rule.Field_count
@@ -37,6 +67,6 @@ func (rule *AuditRuleData) toWireFormat() []byte {
 	*(*[AUDIT_MAX_FIELDS]uint32)(unsafe.Pointer(&b[524:780][0])) = rule.Values
 	*(*[AUDIT_MAX_FIELDS]uint32)(unsafe.Pointer(&b[780:1036][0])) = rule.Fieldflags
 	*(*uint32)(unsafe.Pointer(&b[1036:1040][0])) = rule.Buflen
-	copy(b[1040:1040+rule.Buflen], rule.Buf[:])
+	copy(b[1040:1040+len(rule.Buf)], rule.Buf[:])
 	return b
 }
