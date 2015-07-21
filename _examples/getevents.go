c32faee012d61d27a2a914555233f0fc6a82b80e
Albert Puigsech <albert@puigsech.com>
2015-07-21 15:05:48 +0200

    audir event monitoring using callback function

--- _examples/getevents.go
+++ _examples/getevents.go
@@ -3,47 +3,24 @@ package main
 import (
 	"log"
 	"os"
-	"syscall"
+	"time"
 
 	"github.com/apuigsech/netlink"
 	"github.com/apuigsech/netlink/protocols/audit"
 )
 
+func EventCallback(ae *audit.AuditEvent, ce chan error, args ...interface{}) {
+	log.Println(ae)
+}
+
+
 func main() {
 	logger := log.New(os.Stdout, "", log.LstdFlags)
 	netlink.Logger = logger
 
-	al, _ := audit.OpenLink(0, 0)
+	al,_ := audit.OpenLink(0, 0)
 
-	rule := &audit.AuditRuleData{
-		Flags:  audit.AUDIT_FILTER_EXIT,
-		Action: audit.AUDIT_ALWAYS,
-	}
-	rule.SetSyscall(syscall.SYS_FORK)
-	rule.SetSyscall(syscall.SYS_CLONE)
-	al.AddRule(rule)
+	al.StartEventMonitor(EventCallback, nil)
 
-	st, _ := al.GetStatus()
-	logger.Println("  -", st)
-
-	st.Mask = audit.AUDIT_STATUS_ENABLED | audit.AUDIT_STATUS_PID
-	st.Enabled = 1
-	st.Pid = uint32(os.Getpid())
-
-	al.SetStatus(st)
-	logger.Println("  -", st)
-
-	for {
-		msgList, _ := al.Reply(0)
-		logger.Println("count:", len(msgList))
-		for _, msg := range msgList {
-			if msg.Header.Type == audit.AUDIT_SYSCALL {
-				logger.Printf("%s", msg.Data)
-			} else {
-				logger.Println(msg)
-			}
-		}
-	}
-
-	al.DelRule(rule)
+	time.Sleep(3600 * time.Second)
 }
